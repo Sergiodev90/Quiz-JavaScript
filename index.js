3a261f1ec701a5076a674e51b97e15a7c95f6058
sergio <sergionoriegacantillo@gmail.com>
2024-03-08 21:26:37 +0100

    add new packages and setting up new foldes and files: views, partials, index.js

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 import express  from "express";
-
+import expressEjsLayouts from "express-ejs-layouts";
 const app = express()
 const PORT = 3000
+
+
+app.use(express.static("public"))
+
+
+app.use(expressEjsLayouts)
+app.set('layout','/layouts/main')
+app.set('view engine','ejs')
+
+app.use('/',mainRouter)
 
 app.listen(PORT,()=>{
     console.log("App runing succesfully at http://localhost:3000")
