71c5187de80bc6184314b683683602b803856dbc
sergio <sergionoriegacantillo@gmail.com>
2024-03-11 13:15:37 +0100

    add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './views/partials/**/*.ejs',
+    './views/**/*.ejs',
+    './views/layouts/**/*-ejs'
+   ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
